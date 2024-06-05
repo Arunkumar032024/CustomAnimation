b90b027f22f205233a52e1bdc39ec35b9842e477
Arunkumar032024 <arunkumar032024@gmail.com>
2024-06-05 12:49:02 +0530

    fix bug

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-    alert("hi")
-    $(window).on("load", function(){
-        $(".loader-container").fadeOut(1500);
-        alert("hi")
-    })
+$(window).on("load", function () {
+    $(".loader-container").fadeOut(1500);
 })
